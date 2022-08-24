0b555d01e968ea72e0cd966e08d8b9c05d3be1b8
Guilherme Cintra <gui7lk@gmail.com>
2022-08-24 17:59:46 -0300

    Mensagens já estão sendo recebidas e exibidas

--- js/Index.js
+++ js/Index.js
@@ -3,6 +3,7 @@ const loginScreen = document.getElementById('Login');
 const urlLogin = 'https://mock-api.driven.com.br/api/v6/uol/participants';
 const urlConnection = 'https://mock-api.driven.com.br/api/v6/uol/status';
 const urlMessages = 'https://mock-api.driven.com.br/api/v6/uol/messages';
+const main = document.querySelector('main');
 
 function login(userName){
     Login = {name: userName.value};
@@ -17,6 +18,8 @@ function login(userName){
 
 function logged(){
     loginScreen.classList.add('Hide');
+    const messages = axios.get(urlMessages);
+    messages.then(showMessages);
 }
 
 function error(){
@@ -26,6 +29,17 @@ function error(){
 function desconnection(){
     alert('Houve um erro no servidor');
     window.location.reload()
+}
+
+function showMessages(serverMessage){
+    main.innerHTML += `
+                    <div class="Message">
+                        <p><time>${serverMessage.data[0].time} </time><strong>${serverMessage.data[0].from}</strong> para <strong>${serverMessage.data[0].to}:</strong> ${serverMessage.data[0].text}</p>
+                    </div>
+
+                    <div class="Message">
+                        <p><time>${serverMessage.data[1].time} </time><strong>${serverMessage.data[1].from}</strong> para <strong>${serverMessage.data[1].to}:</strong> ${serverMessage.data[1].text}</p>
+                    </div>`;
 }
 
 function toggleMenuParticipants(x){
