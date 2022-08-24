978c563abbd190dd43c22eb539c0c72b55fb2923
Guilherme Cintra <gui7lk@gmail.com>
2022-08-24 13:32:01 -0300

    Mantendo conexão com o servidor

--- js/Index.js
+++ js/Index.js
@@ -1,12 +1,18 @@
 let Login = '';
 const loginScreen = document.getElementById('Login');
-const url = 'https://mock-api.driven.com.br/api/v6/uol/participants';
+const urlLogin = 'https://mock-api.driven.com.br/api/v6/uol/participants';
+const urlConnection = 'https://mock-api.driven.com.br/api/v6/uol/status';
+const urlMessages = 'https://mock-api.driven.com.br/api/v6/uol/messages';
 
 function login(userName){
     Login = {name: userName.value};
-    const loginServer = axios.post(url, Login);
+    let loginServer = axios.post(urlLogin, Login);
     loginServer.then(logged);
     loginServer.catch(error);
+    setInterval(() => {
+        loginServer = axios.post(urlConnection, Login);
+        loginServer.catch(desconnection);
+    }, 5000);
 }
 
 function logged(){
@@ -15,6 +21,11 @@ function logged(){
 
 function error(){
     alert(`Nome de usuário "${Login.name}" já está em uso!`);
+}
+
+function desconnection(){
+    alert('Houve um erro no servidor');
+    window.location.reload()
 }
 
 function toggleMenuParticipants(x){
